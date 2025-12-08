6ad39a450057a2b60b4da58842d4f797e6f5670c
elmerohueso <elmerohueso@gmail.com>
2025-12-07 21:24:34 -0700

    patch bump

--- app.py
+++ app.py
@@ -64,7 +64,7 @@ app.secret_key = os.environ.get('SECRET_KEY', 'dev-secret-key-change-in-producti
 
 # Application version
 
-__version__ = '2.3.2'
+__version__ = '2.3.3'
 
 # Github repo URL
 GITHUB_REPO_URL = 'https://github.com/elmerohueso/FamilyChores'
